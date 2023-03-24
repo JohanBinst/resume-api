f6107502dbb461b57a7a7c684dc1d5721fee0c57
Johan Binst <johan.binst@gmail.com>
2023-03-24 20:36:20 +1100

    added user authentication

--- app/controllers/api/v1/resumes_controller.rb
+++ app/controllers/api/v1/resumes_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ResumesController < ApplicationController
   before_action :set_resume, only: [:show, :update, :destroy]
+  before_action :authenticate_user!, only: [:create, :update, :destroy]
 
   def index
     @resumes = Resume.all
